9509615e274132f1141d41b9f9021e86cff5b5ee
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-13 10:37:51 -0600

    fixed slider to show all photos

--- website/assets/javascript/app.js
+++ website/assets/javascript/app.js
@@ -7,22 +7,23 @@ function rotateImages(){
 
   $("#photoShow").animate({marginLeft: "-800px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-1200px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-1600px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-2000px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-2400px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-2800px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-3200px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-3600px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-4000px"}, 1000).delay(4000);
 
-  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+  $("#photoShow").animate({marginLeft: "-4400px"}, 1000).delay(4000);
 
+  $("#photoShow").animate({marginLeft: "-0px"}, 1000).delay(4000);
 }
