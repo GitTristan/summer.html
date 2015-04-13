446d31bce43aba4596910ae9715ebd383de76944
Tristan Rubadeau <tristan@rubadeau.com>
2015-04-13 08:45:08 -0600

    photo slider working

--- website/assets/javascript/app.js
+++ website/assets/javascript/app.js
@@ -8,4 +8,21 @@ function rotateImages(){
   $("#photoShow").animate({marginLeft: "-800px"}, 1000).delay(4000);
 
   $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
+  $("#photoShow").animate({marginLeft: "0px"}, 1000).delay(4000);
+
 }
